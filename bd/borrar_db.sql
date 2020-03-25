09ae205ad8a1b3fd743a36796a7da13c732b97fe
Mariano Rodríguez Díaz <contacto@marianord.com>
2020-03-24 21:45:09 -0600

    Script para hacer DROP de las tablas.

---
+++ bd/borrar_db.sql
@@ -0,0 +1,8 @@
+DROP TABLE f_usuario;
+DROP TABLE f_cliente;
+DROP TABLE f_empresas;
+DROP TABLE f_direccion_empresa;
+DROP TABLE f_logo;
+DROP TABLE f_factura;
+DROP TABLE f_concepto_facturado;
+DROP TABLE f_concepto;
