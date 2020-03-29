660976bfbe8eb60e1e4b6830bad0b8d5883e7e47
diazrigoberto01 <38387426+diazrigoberto01@users.noreply.github.com>
2020-03-29 12:55:24 -0600

    Update modificaremp_Admin.php

--- sitio_web/admin/modificaremp_Admin.php
+++ sitio_web/admin/modificaremp_Admin.php
@@ -4,40 +4,67 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Modificar Empresa</title>
-
-    <script>
-      function modificar() {
-        var modificar=window.confirm("¿Seguro que desea modificar?");
-        if (modificar) {
-          alert("Modificado con exito");
-          history.go(-1);
-        } else {
-          return 0;
-        }
-      }
-    </script>
   </head>
   <body>
-    <?php
-    include '../comun/conexion.php';
-    $link = Conectarse();
-    $id_empresa = $_POST['id'];
-    $consulta = mysqli_query($link, "SELECT * FROM f_empresas WHERE id = '$id_empresa'");
-    $datos_empresa = mysqli_fetch_array($consulta);
-    $rfc_empresa = $datos_empresa['rfc'];
-    $consulta_direccion = mysqli_query($link, "SELECT * FROM f_direccion_empresa WHERE empresa_rfc = '$rfc_empresa'");
-    $datos_direccion = mysqli_fetch_array($consulta_direccion);
-    ?>
     <h1>Modificar Empresa</h1>
+
+    <?php
+    if($_GET["id"]){
+      
+      $id=$_GET["id"];
+      include '../comun/conexion.php';//
+      $link = Conectarse();
+      $consulta = mysqli_query($link, "SELECT e.nombre_comercial, e.contacto, e.rfc, e.telefono,e.email,de.calle,de.colonia,de.municipio,de.estado,de.pais,de.n_exterior,de.cp FROM f_empresas e inner join f_direccion_empresa de on (e.rfc=de.empresa_rfc)  where e.id='$id'");
+      $row = mysqli_fetch_array($consulta);
+      echo $row[6];
+    }else{
+      header("Location: empresas_admin.php");
+    }
+    if($_POST){
+       $rfc=$_POST["razon_social"];
+        $nombreComercial=$_POST["nombre_comercial"];
+        $contacto=$_POST["contacto_nombre"];
+        $telefono=$_POST["telefono"];
+        $email=$_POST["email"];
+        $pais=$_POST["pais"];
+        $estado=$_POST["estado"];
+        $municipio=$_POST["municipio"];
+        $calle=$_POST["calle"];
+        $n_ext=$_POST["n_ext"];
+        $n_int=$_POST["n_int"];
+        $localidad=$_POST["localidad"];
+        $cp=$_POST["cp"];
+        
+        $imagen=$_POST["imagen"];
+
+        $update1=mysqli_query($link, "UPDATE f_empresas SET nombre_comercial='$nombreComercial', contacto='$contacto', rfc='$rfc', telefono='$telefono',email='$email' where id='$id'");
+        $update2=mysqli_query($link, "UPDATE f_direccion_empresa set calle='$calle',colonia='$localidad',municipio='$municipio',estado='$estado',pais='$pais',n_exterior='$n_ext',cp=$cp,localidad='$localidad'  where empresa_rfc='$row[2]'");
+        if($update1 and $update2){
+          echo "<script>alert('Actualizacion correcta');
+          location.href=empresas_admin.php;
+          </script>";
+         
+        }else{
+          echo "<script> alert('Algo salio mal')</script>";
+        }
+
+      }
+       
+
+
+  
+    ?>
     <form action="" method="POST">
       <table>
         <tr>
-          <td>Nombre o Razon Social</td>
+          <td>RFC</td>
           <td colspan="2">
             <input
               type="text"
               name="razon_social"
-              value="<?php echo $datos_empresa['razon_social']; ?>"
+              id=""
+              value="<?php echo $row[2]?>"
+              placeholder="Ingresa la razon social"
               size="43"
             />
           </td>
@@ -49,7 +76,8 @@
             <input
               type="text"
               name="nombre_comercial"
-              value="<?php echo $datos_empresa['nombre_comercial']; ?>"
+              value="<?php echo $row[0]?>"
+              placeholder="Ingresa el nombre Comercial"
               size="43"
             />
           </td>
@@ -60,7 +88,9 @@
             <input
               type="text"
               name="contacto_nombre"
-              value="<?php echo $datos_empresa['contacto']; ?>"
+              value="<?php echo $row[1]?>"
+              id=""
+              placeholder="Nombre"
             />
           </td>
         </tr>
@@ -70,62 +100,67 @@
             <input
               type="text"
               name="telefono"
-              value="<?php echo $datos_empresa['telefono']; ?>"
+              id=""
+              value="<?php echo $row[3]?>"
+              placeholder="Telefono fijo"
             />
           </td>
         </tr>
         <tr>
           <td>Email:</td>
-          <td><input type="text" name="email" value="<?php echo $datos_empresa['email']; ?>" /></td>
+          <td><input type="text" name="email" id="" placeholder="Email" value="<?php echo $row[4]?>" /></td>
         </tr>
         <tr>
           <td>Pais</td>
-          <td><input type="text" name="pais" value="<?php echo $datos_direccion['pais']; ?>" /></td>
+          <td><input type="text" name="pais" id="" placeholder="Pais" value="<?php echo $row[9]?>"/></td>
         </tr>
         <tr>
           <td>Estado</td>
           <td>
-            <input type="text" name="estado" value="<?php echo $datos_direccion['estado']; ?>" />
+            <input type="text" name="estado" id="" placeholder="Estado" value="<?php echo $row[8]?>"/>
           </td>
         </tr>
         <tr>
           <td>Municipio</td>
           <td>
-            <input type="text" name="municipio" value="<?php echo $datos_direccion['municipio']; ?>" />
-          </td>
-        </tr>
-        <tr>
-          <td>Localidad</td>
-          <td>
-            <input type="text" name="localidad" value="<?php echo $datos_direccion['localidad']; ?>" />
+            <input type="text" name="municipio" id="" placeholder="Municipio" value="<?php echo $row[7]?>"/>
           </td>
         </tr>
         <tr>
           <td>Calle</td>
-          <td><input type="text" name="calle" value="<?php echo $datos_direccion['calle']; ?>" /></td>
+          <td><input type="text" name="calle" id="" placeholder="Calle" value="<?php echo $row[5]?>"/></td>
         </tr>
         <tr>
           <td>Nº Exterior</td>
           <td>
-            <input type="text" name="n_ext" value="<?php echo $datos_direccion['n_exterior']; ?>" />
+            <input type="text" name="n_ext" id="" placeholder="Nº Exterior" value="<?php echo $row[10]?>"/>
           </td>
         </tr>
         <tr>
           <td>Nº Interior</td>
           <td>
-            <input type="text" name="n_int" value="<?php echo $datos_direccion['n_interior']; ?>" />
+            <input type="text" name="n_int" id="" placeholder="Nº Interior" />
+          </td>
+        </tr>
+        <tr>
+          <td>Localidad</td>
+          <td>
+            <input type="text" name="localidad" id="" placeholder="Localidad" value="<?php echo $row[6]?>"/>
           </td>
         </tr>
         <tr>
           <td>CP</td>
-          <td><input type="text" name="cp" value="<?php echo $datos_direccion['cp']; ?>" /></td>
+          <td><input type="text" name="cp" id="" placeholder="CP" value="<?php echo $row[11]?>"/></td>
         </tr>
+        
         <tr>
           <td>Imagen:</td>
           <td colspan="2">
-            <input type="file" name="imagen" placeholder="Sube aqui el logo" disabled>
+            <input type="file" name="imagen" id="" placeholder="Sube aqui el logo">
           </td>
         </tr>
+
+
       <tr>
         <td colspan="2">
           <center>
