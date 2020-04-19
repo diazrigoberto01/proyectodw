772f60f13458529e3a88c5005859003f53f60885
Mariano Rodríguez Díaz <contacto@marianord.com>
2020-04-19 18:45:45 -0500

    Agregar la información inicial para el index y sobre nosotros.

--- sitio_v2/index.php
+++ sitio_v2/index.php
@@ -8,7 +8,17 @@
   <body>
     <?php require "comun/navbar.php" ?>
     <div class="container-fluid">
-      Contenido aquí.
+      <div class="row align-items-center justify-content-center pt-4 pb-4">
+        <h2>Olvídate de los sistemas complicados para hacer facturas.</h2>
+      </div>
+      <div class="row align-items-center justify-content-center pt-4 pb-4">
+        <img src="img/splash.jpg" alt="Splash art" width="80%" height="80%">
+      </div>
+      <div class="row">
+        <p>
+          En Factura Fácil nos encargamos de facilitarte el proceso de generación de la factura, para que tu negocio no se detenga por no poder cobrarle a los clientes.
+        </p>
+      </div>
     </div>
     <?php require "comun/footer.php" ?>
   </body>
--- sitio_v2/sobre-nosotros.php
+++ sitio_v2/sobre-nosotros.php
@@ -8,7 +8,44 @@
   <body>
     <?php require "comun/navbar.php" ?>
     <div class="container-fluid">
-      Contenido aquí.
+      <div class="row align-items-center justify-content-center mb-4">
+        <div class="card" style="width: 18rem;">
+          <img src="img/logo.png" class="card-img-top" alt="img/logo.png">
+          <div class="card-body">
+            <h5 class="card-title">Mariano Rodríguez Díaz</h5>
+            <p class="card-text">Hace sus cosas.</p>
+            <div class="col-12 mx-auto">
+              <i style="margin-right: 10px;" class="fa fa-facebook fa-3x"></i>
+              <i style="margin-right: 10px;" class="fa fa-github fa-3x"></i>
+              <i style="margin-right: 10px;" class="fa fa-whatsapp fa-3x"></i>
+            </div>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img src="img/logo.png" class="card-img-top" alt="img/logo.png">
+          <div class="card-body">
+            <h5 class="card-title">Rigoberto Martinez Diaz</h5>
+            <p class="card-text">Hace sus cosas, pero son otras cosas.</p>
+            <div class="col-12 mx-auto">
+              <i style="margin-right: 10px;" class="fa fa-facebook fa-3x"></i>
+              <i style="margin-right: 10px;" class="fa fa-github fa-3x"></i>
+              <i style="margin-right: 10px;" class="fa fa-whatsapp fa-3x"></i>
+            </div>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img src="img/logo.png" class="card-img-top" alt="img/logo.png">
+          <div class="card-body">
+            <h5 class="card-title">Ma. Fernanda Paredes Hernandez</h5>
+            <p class="card-text">Hace cosas, pero aún otras cosas a las otras cosas que hace uno y distintas a las del otro.</p>
+            <div class="col-12 mx-auto">
+              <i style="margin-right: 10px;" class="fa fa-facebook fa-3x"></i>
+              <i style="margin-right: 10px;" class="fa fa-github fa-3x"></i>
+              <i style="margin-right: 10px;" class="fa fa-whatsapp fa-3x"></i>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <?php require "comun/footer.php" ?>
   </body>
