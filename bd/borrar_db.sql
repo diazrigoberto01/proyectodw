dfb1022832c1aa4e9af673cdc9771f935f7d5bca
diazrigoberto01 <38387426+diazrigoberto01@users.noreply.github.com>
2020-04-30 18:23:36 -0500

    Update borrar_db.sql

--- bd/borrar_db.sql
+++ bd/borrar_db.sql
@@ -1,9 +1,9 @@
 DROP TABLE f_usuario;
-DROP TABLE f_cliente;
 DROP TABLE f_concepto_facturado;
 DROP TABLE f_factura;
 DROP TABLE f_logo;
 DROP TABLE f_direccion_empresa;
 DROP TABLE f_empresas;
+DROP TABLE f_cliente;
 DROP TABLE f_concepto;
 DROP TABLE f_usoscfdi;
