79dd755eb97a79241a39336101a8b834725f8a60
diazrigoberto01 <38387426+diazrigoberto01@users.noreply.github.com>
2020-04-30 18:02:28 -0500

    Update direccion_empresas.sql

--- bd/datos_demo/direccion_empresas.sql
+++ bd/datos_demo/direccion_empresas.sql
@@ -1,7 +1,7 @@
-INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc) VALUES ('30 poniente','centro','Puebla','Puebla','Mexico','309','72160','RFCEMP2309SLF','132asd');
-INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc) VALUES ('juarez norte','centro','Pachuca','hidalgo','Mexico','9','89029','HFURB238948SH7','132asd');
-INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc) VALUES ('2 poniente','centro','Puebla','Puebla','Mexico','1009','72160','FER457HFU8DS','132asd');
+INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc,f_empresas_id) VALUES ('30 poniente','centro','Puebla','Puebla','Mexico','309','72160','RFCEMP2309SLF','132asd','1');
+INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc,f_empresas_id) VALUES ('juarez norte','centro','Pachuca','hidalgo','Mexico','9','89029','HFURB238948SH7','132asd','2');
+INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc,f_empresas_id) VALUES ('2 poniente','centro','Puebla','Puebla','Mexico','1009','72160','FER457HFU8DS','132asd','3');
 -- Duplicados
-INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc) VALUES ('30 poniente','centro','Puebla','Puebla','Mexico','309','72160','RFCEMP2309SLF','132asd');
-INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc) VALUES ('juarez norte','centro','Pachuca','hidalgo','Mexico','9','89029','HFURB238948SH7','132asd');
-INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc) VALUES ('2 poniente','centro','Puebla','Puebla','Mexico','1009','72160','FER457HFU8DS','132asd');
+INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc,f_empresas_id) VALUES ('30 poniente','centro','Puebla','Puebla','Mexico','309','72160','RFCEMP2309SLF','132asd','4');
+INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc,f_empresas_id) VALUES ('juarez norte','centro','Pachuca','hidalgo','Mexico','9','89029','HFURB238948SH7','132asd','5');
+INSERT INTO f_direccion_empresa(calle,colonia,municipio,estado,pais,n_exterior,cp,empresa_rfc,empresa_usuario_rfc,f_empresas_id) VALUES ('2 poniente','centro','Puebla','Puebla','Mexico','1009','72160','FER457HFU8DS','132asd','6');
