aee8b14a3a9f28223a9c3d5f3fddaa3554d8e565
diazrigoberto01 <38387426+diazrigoberto01@users.noreply.github.com>
2020-04-30 17:57:59 -0500

    Update logos.sql

--- bd/datos_demo/logos.sql
+++ bd/datos_demo/logos.sql
@@ -1,7 +1,7 @@
-INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc) VALUES ('logo1','Logo construccion lopez','2000/05/03','RFCEMP2309SLF');
-INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc) VALUES ('logoabc','Logo arrendadora ','2012/09/03','HFURB238948SH7');
-INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc) VALUES ('logo21','Logo del bosque','2019/10/04','FER457HFU8DS');
+INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc,f_empresas_id) VALUES ('logo1','Logo construccion lopez','2000/05/03','RFCEMP2309SLF');
+INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc,f_empresas_id) VALUES ('logoabc','Logo arrendadora ','2012/09/03','HFURB238948SH7');
+INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc,f_empresas_id) VALUES ('logo21','Logo del bosque','2019/10/04','FER457HFU8DS');
 -- Duplicados
-INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc) VALUES ('logo1','Logo construccion lopez','2000/05/03','RFCEMP2309SLF');
-INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc) VALUES ('logoabc','Logo arrendadora ','2012/09/03','HFURB238948SH7');
-INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc) VALUES ('logo21','Logo del bosque','2019/10/04','FER457HFU8DS');
+INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc,f_empresas_id) VALUES ('logo1','Logo construccion lopez','2000/05/03','RFCEMP2309SLF');
+INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc,f_empresas_id) VALUES ('logoabc','Logo arrendadora ','2012/09/03','HFURB238948SH7');
+INSERT INTO f_logo(imagen,nombre,fecha,empresa_rfc,f_empresas_id) VALUES ('logo21','Logo del bosque','2019/10/04','FER457HFU8DS');
