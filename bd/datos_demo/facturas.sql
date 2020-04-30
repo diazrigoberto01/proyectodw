8b638c168af29b5414dcad59a3781df0c1f4f150
diazrigoberto01 <38387426+diazrigoberto01@users.noreply.github.com>
2020-04-30 18:30:20 -0500

    Update facturas.sql

--- bd/datos_demo/facturas.sql
+++ bd/datos_demo/facturas.sql
@@ -3,7 +3,7 @@ INSERT INTO f_factura(folio,rfc_emisor,direccion_emisor,lugar_expedicion,fecha_e
   VALUES (31,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-24','123RFC456',
   'Transferencia',167.04,'123asd',23,144,'AdquisiciÃ³n de mercancias','Pago unico','1','1');
 INSERT INTO f_factura(folio,rfc_emisor,direccion_emisor,lugar_expedicion,fecha_emision,
-  rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) VALUES (32'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-24','123RFC456','Transferencia',116,'123asd',16,100,'AdquisiciÃ³n de mercancias','Pago unico','1','1');
+  rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) VALUES (32,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-24','123RFC456','Transferencia',116,'123asd',16,100,'AdquisiciÃ³n de mercancias','Pago unico','1','1');
 INSERT INTO f_factura(folio,rfc_emisor,direccion_emisor,lugar_expedicion,fecha_emision,
   rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) VALUES (33,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-24','123RFC456','Transferencia',3281.64,'123asd',453,2829,'AdquisiciÃ³n de mercancias','Pago unico','1','2');
 INSERT INTO f_factura(folio,rfc_emisor,direccion_emisor,lugar_expedicion,fecha_emision,
