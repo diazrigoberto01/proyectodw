58eece50b955eda1fe6e07d300bc37a878afda1c
Mariano Rodríguez Díaz <contacto@marianord.com>
2020-05-02 22:38:09 -0500

    Quitar archivos viejos no utilizados. Arreglar el insert para facturas.

--- bd/datos_demo/facturas.sql
+++ bd/datos_demo/facturas.sql
@@ -1,12 +1,10 @@
-INSERT INTO f_factura(folio,rfc_emisor,direccion_emisor,lugar_expedicion,fecha_emision,
-  rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) 
+INSERT INTO f_factura(folio,rfc_emisor,direccion_emisor,lugar_expedicion,fecha_emision,rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id)
   VALUES (31,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-24','123RFC456',
-  'Transferencia',167.04,'123asd',23,144,'AdquisiciÃ³n de mercancias','Pago unico','1','1');
+  'Transferencia',167.04,'123asd',23,144,'Adquisición de mercancias','Pago unico','1',1,1);
+INSERT INTO f_factura(folio,rfc_emisor,direccion_emisor,lugar_expedicion,fecha_emision,rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) VALUES (32,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-24','123RFC456','Transferencia',116,'123asd',16,100,'AdquisiciÃ³n de mercancias','Pago unico','1',1,2);
 INSERT INTO f_factura(folio,rfc_emisor,direccion_emisor,lugar_expedicion,fecha_emision,
-  rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) VALUES (32,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-24','123RFC456','Transferencia',116,'123asd',16,100,'AdquisiciÃ³n de mercancias','Pago unico','1','1');
+  rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) VALUES (33,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-24','123RFC456','Transferencia',3281.64,'123asd',453,2829,'AdquisiciÃ³n de mercancias','Pago unico','1',2,3);
 INSERT INTO f_factura(folio,rfc_emisor,direccion_emisor,lugar_expedicion,fecha_emision,
-  rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) VALUES (33,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-24','123RFC456','Transferencia',3281.64,'123asd',453,2829,'AdquisiciÃ³n de mercancias','Pago unico','1','2');
+  rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) VALUES (34,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-24','123RFC456','Transferencia',1670.4,'123asd',230,1440,'AdquisiciÃ³n de mercancias','Pago unico','1',1,2);
 INSERT INTO f_factura(folio,rfc_emisor,direccion_emisor,lugar_expedicion,fecha_emision,
-  rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) VALUES (34,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-24','123RFC456','Transferencia',1670.4,'123asd',230,1440,'AdquisiciÃ³n de mercancias','Pago unico','1','1');
-INSERT INTO f_factura(folio,rfc_emisor,direccion_emisor,lugar_expedicion,fecha_emision,
-  rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) VALUES (35,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-26','123RFC456','Tarjeta',17344.3,'123asd',2392,14952,'AdquisiciÃ³n de mercancias','3 meses','1','1');
+  rfc_receptor,metodo_pago,importe_total,empresa_usuario_rfc,iva,subtotal,uso_cfdi,cantidadPagos,status,f_empresas_id,f_cliente_id) VALUES (35,'MADR950302SL6','Roberto Covarrubias Norte 302','Tlaxcala','2020-04-26','123RFC456','Tarjeta',17344.3,'123asd',2392,14952,'AdquisiciÃ³n de mercancias','3 meses','1',1,1);
--- sitio_web/comun/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-  function Conectarse() {
-    if (!($link = mysqli_connect("localhost", "proydweb_p2020", "Dw3bp@020", "proydweb_P2020"))) {
-      die('Error conectando: ' . mysqli_connect_error());
-      exit();
-    }
-    return $link;
-  }
-?>
